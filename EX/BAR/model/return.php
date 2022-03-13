0ba93441ea4440f7770abc108715fba058eb50b1
Mezali <danieldelimamazali@hotmail.com>
2022-03-13 15:45:10 -0300

    erros corrigidos. COMPLETO

--- EX/BAR/model/return.php
+++ EX/BAR/model/return.php
@@ -2,27 +2,32 @@
 
 $NOME = $_REQUEST['NOME'];
 $BEBIDA = $_REQUEST['BEBIDA'];
-$NASC = ($_REQUEST['ANO']);
-$DataAtual = date("d/m/Y");
-$IDADE = date_diff(date_create($NASC), date_create($DataAtual));
+$NASC = $_REQUEST['ANO'];
+$DataAtual = date("Y-m-d");
+$IDADE = date_diff(date_create($NASC), date_create($hoje));
 
-echo $DataAtual;
+
+//echo 'Idade é '.$IDADE->format('%y');
+//echo $DataAtual;
 
 if (empty($NOME)) {
     $dados = array(
-        "tipo" => 'error',
         "mensagem" => 'Existe(m) campo(s) a ser(em) preenchido(s).'
     );
 } else {
-    if ($IDADE >= 18) {
+
+    if ($IDADE->format('%y') >= 18) {
         $dados = array(
-            "mensagem" => 'Bem vindo, ' . $NOME . ', A sua bebida favorita é: '. $BEBIDA
+            "B4" => 'alert-success',
+            "mensagem" => 'Bem vindo, ' . $NOME . ', A sua bebida favorita é: ' . $BEBIDA
         );
-    }else{
+    } else {
         $dados = array(
-            "mensagem" => 'você não deveria estar aqui! ' . $NOME . ' você é de menor!'
+            "B4" => 'alert-danger',
+            "mensagem" => 'você não deveria estar aqui! ' . $NOME . ', você é de menor!'
         );
     }
+
 }
 
 echo json_encode($dados);
