9e3f1ef528a01af0d17dd01ea135fb21ef30e518
Mezali <danieldelimamazali@hotmail.com>
2022-03-15 11:29:48 -0300

    COMPLETO

--- EX/BAR/model/return.php
+++ EX/BAR/model/return.php
@@ -13,10 +13,19 @@ $IDADE = date_diff(date_create($NASC), date_create($DataAtual));
 if (empty($NOME)) {
     $dados = array(
         "B4" => 'alert-danger',
-        "mensagem" => 'Existe(m) campo(s) a ser(em) preenchido(s).'
+        "mensagem" => 'INSIRA O SEU NOME!'
+    );
+} else if (empty($IDADE)) {
+    $dados = array(
+        "B4" => 'alert-danger',
+        "mensagem" => 'INSIRA A DATA DE NASCIMENTO'
+    );
+} elseif (empty($BEBIDA)) {
+    $dados = array(
+        "B4" => 'alert-danger',
+        "mensagem" => 'INSIRA A SUA BEBIDA FAVORITA!'
     );
 } else {
-
     if ($IDADE->format('%y') >= 18) {
         $dados = array(
             "B4" => 'alert-success',
@@ -28,7 +37,6 @@ if (empty($NOME)) {
             "mensagem" => 'você não deveria estar aqui! ' . $NOME . ', você é de menor!'
         );
     }
-
 }
 
 echo json_encode($dados);
